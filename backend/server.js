fb608619cbf63ec47dc08930d0e091109075ac19
vedaXD <vedapatki1@gmail.com>
2025-04-18 23:45:49 +0530

    frontendd wohooo

--- backend/server.js
+++ backend/server.js
@@ -5,19 +5,16 @@ import connectDB from './config/db.js';
 import authRoutes from './routes/auth_routes.js';
 import playerRoutes from './routes/player_routes.js';
 
-// Load env variables
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Connect to DB
 connectDB();
 
 app.use('/api/auth', authRoutes);
 app.use('/api/players', playerRoutes);
 
-// Use PORT from .env
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
